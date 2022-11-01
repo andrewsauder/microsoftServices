0c9346ccc668b0490695ef3ce8b8f232d5b3d697
Andrew Sauder <andrewsauder@gmail.com>
2022-11-01 11:34:52 -0400

    adjust code formatting

--- src/components/envelope.php
+++ src/components/envelope.php
@@ -6,25 +6,25 @@ namespace andrewsauder\microsoftServices\components;
 class envelope {
 
 	/** @OA\Property() */
-	public bool   $error   = false;
+	public bool $error = false;
 
 	/** @OA\Property() */
 	public string $message = '';
 
 	/** @OA\Property() */
-	public int    $status  = 0;
+	public int $status = 0;
 
 	/** @OA\Property() */
-	public array  $data    = [];
+	public array $data = [];
 
 
 	/**
 	 * envelope constructor.
 	 *
-	 * @param  int     $status
-	 * @param  bool    $error
-	 * @param  string  $message
-	 * @param  array   $data
+	 * @param int    $status
+	 * @param bool   $error
+	 * @param string $message
+	 * @param array  $data
 	 */
 	public function __construct( int $status = 200, bool $error = false, string $message = '', array $data = [] ) {
 
