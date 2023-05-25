8f18da701e7438ab2d43f8ae35b7a58671f6816b
Andrew Sauder <andrewsauder@gmail.com>
2023-05-25 08:39:12 -0400

    feat: add user lookup by upn and external id

--- src/user.php
+++ src/user.php
@@ -26,14 +26,14 @@ class user extends \andrewsauder\microsoftServices\components\service {
 	 * @return \Microsoft\Graph\Model\User[]
 	 * @throws \andrewsauder\microsoftServices\exceptions\serviceException
 	 */
-	public function allUsersInOrganization( ?string $nextLink=null ): array {
+	public function allUsersInOrganization( ?string $nextLink = null ): array {
 		try {
-			if($nextLink===null) {
+			if( $nextLink===null ) {
 				$nextLink = '/users';
 			}
-			$graphResponse = $this->graph->createRequest( 'GET', $nextLink  )->execute();
-			if($graphResponse->getNextLink()) {
-				error_log($graphResponse->getNextLink());
+			$graphResponse = $this->graph->createRequest( 'GET', $nextLink )->execute();
+			if( $graphResponse->getNextLink() ) {
+				error_log( $graphResponse->getNextLink() );
 				return array_merge( $graphResponse->getResponseAsObject( \Microsoft\Graph\Model\User::class ), $this->allUsersInOrganization( $graphResponse->getNextLink() ) );
 			}
 		}
@@ -53,7 +53,7 @@ class user extends \andrewsauder\microsoftServices\components\service {
 	/**
 	 * @throws \andrewsauder\microsoftServices\exceptions\serviceException
 	 */
-	public function getUserByEmail( string $email ): \Microsoft\Graph\Model\User {
+	public function getUserByUserPrincipalName( string $upn ): \Microsoft\Graph\Model\User {
 		try {
 			//get application access token
 			$accessToken = $this->getMicrosoftAccessToken();
@@ -61,7 +61,7 @@ class user extends \andrewsauder\microsoftServices\components\service {
 			$graph = new \Microsoft\Graph\Graph();
 			$graph->setAccessToken( $accessToken );
 
-			$response = $graph->createRequest( 'GET', '/users/' . $email )->setReturnType( \Microsoft\Graph\Model\User::class )->execute();
+			$response = $graph->createRequest( 'GET', '/users/' . $upn )->setReturnType( \Microsoft\Graph\Model\User::class )->execute();
 		}
 		catch( GraphException $e ) {
 			error_log( $e );
@@ -72,6 +72,23 @@ class user extends \andrewsauder\microsoftServices\components\service {
 			throw new serviceException( 'Failed to get user', $e->getCode(), $e );
 		}
 		return $response;
+	}
+
+
+	/**
+	 * @throws \andrewsauder\microsoftServices\exceptions\serviceException
+	 */
+	public function getUserByExternalId( string $externalId ): \Microsoft\Graph\Model\User {
+		return $this->getUserByUserPrincipalName( $externalId );
+	}
+
+
+	/**
+	 * @throws \andrewsauder\microsoftServices\exceptions\serviceException
+	 * @deprecated use getUserByUserPrincipalName instead. This function has always operate on UPN, not email. Preserved in v2 for backwards compatibility only. Will be removed in v3 or changed to actually search for user by mail attribute
+	 */
+	public function getUserByEmail( string $email ): \Microsoft\Graph\Model\User {
+		return $this->getUserByUserPrincipalName( $email );
 	}
 
 
